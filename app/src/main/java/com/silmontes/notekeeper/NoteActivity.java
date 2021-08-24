07a5b130691c1106039d6eb6617e38da5a225bb1
SilMontes <Silmontes2001@gmail.com>
2021-08-24 17:01:42 -0600

    Third Commit

--- app/src/main/java/com/silmontes/notekeeper/NoteActivity.java
+++ app/src/main/java/com/silmontes/notekeeper/NoteActivity.java
@@ -31,7 +31,7 @@ public class NoteActivity extends AppCompatActivity {
     private int mNotePosition;
     private boolean misCancelling;
     private NoteActivityViewModel mViewModel;
-//note
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
