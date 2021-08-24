bc4d2a61c91e9043f772c19096f653489502eb60
SilMontes <Silmontes2001@gmail.com>
2021-08-24 17:02:56 -0600

    New Upgrade

--- app/src/main/java/com/silmontes/notekeeper/NoteActivity.java
+++ app/src/main/java/com/silmontes/notekeeper/NoteActivity.java
@@ -31,7 +31,7 @@ public class NoteActivity extends AppCompatActivity {
     private int mNotePosition;
     private boolean misCancelling;
     private NoteActivityViewModel mViewModel;
-
+// new upgrade
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
