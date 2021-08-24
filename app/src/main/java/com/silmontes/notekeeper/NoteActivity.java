7c80ccbd3f18a06b6867850bb351661ebc7dd347
SilMontes <Silmontes2001@gmail.com>
2021-08-24 16:55:10 -0600

    First Commit

---
+++ app/src/main/java/com/silmontes/notekeeper/NoteActivity.java
@@ -0,0 +1,189 @@
+package com.silmontes.notekeeper;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.PersistableBundle;
+import android.view.Menu;
+
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.List;
+
+public class NoteActivity extends AppCompatActivity {
+
+    public static final String NOTE_POSITION = "com.silmontes.notekeeper.NOTE_POSITION";
+    public static final int POSITION_NOT_SET = -1;
+    private NoteInfo mNote;
+    private boolean mIsNewNote;
+    private Spinner mSpinnerCourses;
+    private EditText mTextNoteTitle;
+    private EditText mTextNoteText;
+    private int mNotePosition;
+    private boolean misCancelling;
+    private NoteActivityViewModel mViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_note);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ViewModelProvider viewModelProvider = new ViewModelProvider(getViewModelStore(),
+         ViewModelProvider.AndroidViewModelFactory.getInstance(getApplication()));
+        mViewModel = viewModelProvider.get(NoteActivityViewModel.class);
+
+        if (mViewModel.mIsNewlyCreated && savedInstanceState != null)
+            mViewModel.restoreState(savedInstanceState);
+
+        mViewModel.mIsNewlyCreated = false;
+
+        mSpinnerCourses = findViewById(R.id.spinner_courses);
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+
+        ArrayAdapter<CourseInfo> adapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,courses);
+        adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        mSpinnerCourses.setAdapter(adapterCourses);
+
+        readDisplayStateValues();
+        saveOriginalNoteValues();
+
+        mTextNoteTitle = findViewById(R.id.text_note_title);
+        mTextNoteText = findViewById(R.id.text_note_text);
+
+        if(!mIsNewNote){
+            displayNote(mSpinnerCourses, mTextNoteTitle, mTextNoteText);
+        }
+
+    }
+
+    private void saveOriginalNoteValues() {
+        if (mIsNewNote)
+            return;
+        mViewModel.mOriginalNoteCourseId = mNote.getCourse().getCourseId();
+        mViewModel.mOriginalNoteTitle = mNote.getTitle();
+        mViewModel.mOriginalNoteText = mNote.getText();
+    }
+
+    private void displayNote(Spinner spinnerCourses, EditText textNoteTitle, EditText textNoteText) {
+
+        List<CourseInfo> courses = DataManager.getInstance().getCourses();
+        int courseIndex = courses.indexOf(mNote.getCourse());
+        spinnerCourses.setSelection(courseIndex);
+
+        textNoteTitle.setText(mNote.getTitle());
+        textNoteText.setText(mNote.getText());
+
+
+    }
+
+
+    private void readDisplayStateValues() {
+
+        Intent intent = getIntent();
+
+
+        int position = intent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET);
+
+        mIsNewNote = position == POSITION_NOT_SET;
+
+        if(mIsNewNote){
+            createNewNote();
+        }else{
+            mNote=DataManager.getInstance().getNotes().get(position);
+        }
+
+
+    }
+    private void createNewNote(){
+        DataManager dm = DataManager.getInstance();
+        mNotePosition = dm.createNewNote();
+        mNote = dm.getNotes().get(mNotePosition);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_send_mail) {
+            sendEmail();
+            return true;
+        }else if(id==R.id.action_cancel){
+            misCancelling = true;
+            finish();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void sendEmail() {
+
+        CourseInfo course = (CourseInfo) mSpinnerCourses.getSelectedItem();
+        String subject = mTextNoteTitle.getText().toString();
+        String text = "Mira lo que aprendí en el curso de \""+ course.getTitle()+"\"\n"+mTextNoteText.getText().toString();
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("message/rfc2822");
+
+        intent.putExtra(Intent.EXTRA_SUBJECT,subject);
+        intent.putExtra(Intent.EXTRA_TEXT,text);
+
+        startActivity(intent);
+
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(misCancelling){
+            if(mIsNewNote)
+                DataManager.getInstance().removeNote(mNotePosition);
+                storePreviousNoteValues();
+        }else{
+            saveNote();
+        }
+
+    }
+
+    private void saveNote() {
+        mNote.setCourse((CourseInfo)mSpinnerCourses.getSelectedItem());
+        mNote.setTitle((mTextNoteTitle.getText().toString()));
+        mNote.setText(mTextNoteText.getText().toString());
+
+    }
+    private void storePreviousNoteValues() {
+        CourseInfo course = DataManager.getInstance().getCourse(mViewModel.mOriginalNoteCourseId);
+        mNote.setCourse(course);
+        mNote.setTitle(mViewModel.mOriginalNoteTitle);
+        mNote.setText(mViewModel.mOriginalNoteText);
+    }
+
+    @Override
+    public void onSaveInstanceState( Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (outState != null)
+            mViewModel.saveState(outState);
+    }
+}
